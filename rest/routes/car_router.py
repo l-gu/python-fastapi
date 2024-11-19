ea2f69119e27489e70dedf715830a7462c98d359
l-gu <laurent@telosys.org>
2024-11-19 09:42:33 +0100

    DTO and gitignore

--- rest/routes/car_router.py
+++ rest/routes/car_router.py
@@ -1,3 +1,4 @@
+from typing import List, Optional
 from fastapi import APIRouter, Request, Response, Header
 
 from rest.dto.car_dto import Car
@@ -13,9 +14,18 @@ router = APIRouter()
         # tags: Organizes the routes in Swagger UI under specific headings. Useful for grouping routes logically 
         tags=["cars"],
         responses={200: {"description": "OK."},})
-async def get_all(request: Request):
+async def get_all(request: Request) -> List[Car]:
     user_agent = request.headers.get("user-agent") # get the 'user-agent' header
-    return [{"car_id": 1}, {"car_id": 2}]
+
+    # Result : list of cars
+    list = []
+    for i in range(20):
+        id = i+1
+        car = Car(id=id, name="Car"+str(id), price=id*1000.0)
+        list.append(car)
+
+    # return [{"car_id": 1}, {"car_id": 2}]
+    return list
 
 
 @router.get("/{id}", 
@@ -25,7 +35,7 @@ async def get_all(request: Request):
         responses={
             200: {"description": "OK."},
             404: {"description": "Not found."}, })
-async def get_by_id(request: Request, id: int):
+async def get_by_id(request: Request, id: int) -> Car:
     # Get car by id
     if id > 100:
         # If car is not found, return 404 status
